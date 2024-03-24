afcf20245d8d43162377245b61446c485efe42d8
Andrii Druzhha <xgodkeyx@gmail.com>
2024-03-24 15:55:12 +0200

    New css, add url and base

--- siteRegistrationTest/urls.py
+++ siteRegistrationTest/urls.py
@@ -18,10 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from main.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", main_page, name="main")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
